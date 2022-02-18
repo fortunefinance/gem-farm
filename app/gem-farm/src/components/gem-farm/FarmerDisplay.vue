bf03b867a77dda28706ba81bb8245232b7267d0c
fortunefinance <fortunedotfinance@protonmail.com>
2022-02-18 13:17:20 -0800

    Update FarmerDisplay.vue

--- app/gem-farm/src/components/gem-farm/FarmerDisplay.vue
+++ app/gem-farm/src/components/gem-farm/FarmerDisplay.vue
@@ -11,12 +11,7 @@
     <div class="mb-2">Your identity: {{ farmerAcc.identity.toBase58() }}</div>
     <div class="mb-2">Associated vault: {{ farmerAcc.vault.toBase58() }}</div>
     <div class="mb-2">Cookies staked: {{ farmerAcc.gemsStaked }}</div>
-    <div class="mb-2">
-      Min staking ends: {{ parseDate(farmerAcc.minStakingEndsTs) }}
-    </div>
-    <div class="mb-5">
-      Cooldown ends: {{ parseDate(farmerAcc.cooldownEndsTs) }}
-    </div>
+
 
     <div class="flex mb-5">
       <div class="flex-1 mr-5">
